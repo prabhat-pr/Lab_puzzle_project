82fa032c082e6d9243bebd5f9e024680b1c8af5f
Prabhat Ranjan <128122689+prabhat-pr@users.noreply.github.com>
2023-05-05 10:26:40 +0530

    updated new code

--- AsciiValue.java
+++ AsciiValue.java
@@ -8,9 +8,6 @@ public class AsciiValue {
 
         System.out.println("The ASCII value of "  + ch +"  is "  + ascii);
         System.out.println("The ASCII value of "  + ch + "  is  " + castAscii);
-        System.out.println("The ASCII value of "  + ch +"  is "  + ascii);
-        System.out.println("The ASCII value of "  + ch + "  is  " + castAscii);
-        System.out.println("The ASCII value of "  + ch +"  is "  + ascii);
-        System.out.println("The ASCII value of "  + ch + "  is  " + castAscii);
+       System.out.println("The ASCII value");
     }
 }
