a0ed29518cd2e6b5ec0c76528e58e19bac41e20e
prabhat-pr <prab_24@ymail.com>
2023-05-05 09:24:02 +0530

    changes done

---
+++ activity.java
@@ -0,0 +1,4 @@
+The World Wide Web (WWW), commonly known as the Web, is an information system enabling documents and other web resources to be accessed over the Internet.
+JavaScript (JS) is a lightweight, interpreted, or just-in-time compiled programming language with first-class functions. 
+Java is a high-level, class-based, object-oriented programming language that is designed to have as few implementation dependencies as possible.
+Cascading Style Sheets is a style sheet language used for describing the presentation of a document written in a markup language such as HTML or XML. 
